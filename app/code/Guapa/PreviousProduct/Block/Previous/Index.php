8b1bf5427abb7ec8511cc1d712aeab0675883cc2
MikeDrenth <mdrenth.1990@gmail.com>
2019-01-21 21:47:31 +0100

    [TASK] Block changes to get product info. Template change to show table

--- app/code/Guapa/PreviousProduct/Block/Previous/Index.php
+++ app/code/Guapa/PreviousProduct/Block/Previous/Index.php
@@ -1,21 +1,37 @@
 <?php
 
-
 namespace Guapa\PreviousProduct\Block\Previous;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
+    protected $recentlyViewed;
 
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\View\Element\Template\Context  $context
-     * @param array $data
-     */
-    public function __construct(
+    public function __construct (
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\ProductFactory $productArr,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Reports\Block\Product\Viewed $recentlyViewed,
         array $data = []
     ) {
-        parent::__construct($context, $data);
+        $this->recentlyViewed    = $recentlyViewed;
+        $this->productArr        = $productArr;
+        $this->productRepository = $productRepository;
+        parent::__construct( $context, $data );
+    }
+
+    // Function: Get product name by product ID
+    public function getProduct($id) {
+        return $this->productArr->create()->load($id);
+    }
+    
+    // Function: Get product url by product ID
+    public function getProductUrl($productId){
+        $product = $this->productRepository->getById($productId);
+        return $product->getUrlModel()->getUrl($product);
+    }
+
+    // Function: Get recently visited products
+    public function getMostRecentlyViewed() {
+        return $this->recentlyViewed->getItemsCollection()->getData();
     }
 }
